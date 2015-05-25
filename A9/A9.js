5d232bdbb7829a20c80f6d5226ffeb9631c5d68f
Teo-MidasMedia <teo@midasmedia.co.uk>
2015-05-25 12:40:55 +0100

    Create A9.js

---
+++ A9/A9.js
@@ -0,0 +1,38 @@
+//core modules
+var http = require('http');
+
+//installed modules
+var concatStream = require('concat-stream');
+
+//my vars
+var arr = [];
+var collected = 0;
+
+function getData(i){
+		var urls = process.argv.slice(2);
+		var holder = '';
+		
+		http.get(urls[i], function(response){
+			response.pipe(concatStream(function(data){
+				data = data.toString();
+				holder += data;
+			}));
+			response.on('end', function(){
+				arr[i] = holder;
+				collected ++;
+				if(collected == (process.argv.length - 2)){
+					theResults();
+				}
+			});
+		}); 
+}
+
+function theResults(){
+		arr.forEach(function(data){
+			console.log(data);
+		});
+}
+
+for(var i = 0; i<3; i++) {
+	getData(i);
+}
