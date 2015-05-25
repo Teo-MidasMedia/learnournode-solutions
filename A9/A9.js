f5c7baa70cc6686938dc8c128029444efed7c9f7
Teo-MidasMedia <teo@midasmedia.co.uk>
2015-05-25 12:42:58 +0100

    Update A9.js

--- A9/A9.js
+++ A9/A9.js
@@ -11,26 +11,25 @@ var urls = process.argv.slice(2);
 var holder = '';
 
 function getData(i){
-		
-		http.get(urls[i], function(response){
-			response.pipe(concatStream(function(data){
-				data = data.toString();
-				holder += data;
-			}));
-			response.on('end', function(){
-				arr[i] = holder;
-				collected ++;
-				if(collected == (process.argv.length - 2)){
-					theResults();
-				}
-			});
-		}); 
+	http.get(urls[i], function(response){
+		response.pipe(concatStream(function(data){
+			data = data.toString();
+			holder += data;
+		}));
+		response.on('end', function(){
+			arr[i] = holder;
+			collected ++;
+			if(collected == (process.argv.length - 2)){
+				theResults();
+			}
+		});
+	}); 
 }
 
 function theResults(){
-		arr.forEach(function(data){
-			console.log(data);
-		});
+	arr.forEach(function(data){
+		console.log(data);
+	});
 }
 
 for(var i = 0; i<3; i++) {
